d751681f002057668d0b83cfa8518b8c9959c2c5
Daniel Cezar Chiaveri Demico <DDEMICO@jacto.com.br>
2025-06-23 13:18:56 -0300

    Guessing Game

--- PBS-4/game/game.py
+++ PBS-4/game/game.py
@@ -1,1 +1,32 @@
-#teste
+import random
+
+while True:
+    try:
+        n = int(input("Level: "))
+        break
+    except ValueError:
+        continue
+
+target = random.randint(1,n)
+
+while True:
+    try:
+        g = int(input("Guess: "))
+    except ValueError:
+        continue
+    if g == target:
+        print("just right!")
+        break
+    elif g > target:
+        print("Too large!")
+        continue
+    elif g < target:
+        print("Too Small!")
+        continue
+    else:
+        print("Something Wrong")
+        break
+
+    
+
+    
